8954c5c956fbbcbc53353d90932fb3285fe0e93a
yaky-dev <anton@yaky.dev>
2023-01-21 20:50:50 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# synapse-room-cleaner
-Room cleaner for Matrix Synapse
+# Synapse Room Cleaner
+
+Simple web tool for your Matrix Synapse server that allows you to:
+- List rooms
+- List room members
+- Delete rooms
+
+To use this tool, you need to [set yourself up as admin and get the access token](https://matrix-org.github.io/synapse/latest/usage/administration/admin_api/index.html#authenticate-as-a-server-admin), and your server's Admin API has to be accessible, [which is a risk](https://matrix-org.github.io/synapse/latest/reverse_proxy.html#synapse-administration-endpoints).
+
+
+## How to use
+
+- Open [Synapse Room Cleaner hosted on yaky.dev](https://yaky.dev/apps/synapse-room-cleaner)
+- Enter your homeserver address and your admin access token
+- Click "List Rooms". If your homeserver is reachable and your access token is valid, you will get a table containing a list of all rooms on your server.
+
+For each room:
+- Click "List Members" to get a list of _local_ users who have joined that room
+- Click "Delete" to delete that room (uses the default "purge" option which deletes the room from the database)
+
+
+## Running locally
+
+You don't trust some guy named Yaky with your homeserver and access token, I get it. Or you don't expose your homeserver's API to the web, because you are a careful person.
+
+You can run the tool locally by cloning or downloading the source and opening `synapse-room-cleaner/index.html` in your browser. 
+
+
+## Privacy
+
+This tool runs in your browser, and only communicates to the Admin API of the homeserver that you specify. All API calls use JavaScript's Fetch API and are issued from `index.js`, which is in its original, readable form.
+
+Homeserver address and admin access token are saved to local storage for convenience. To remove them from local storage, simply set them back to blank. 
+
+Synapse Room Cleaner uses [Alpine.js](https://alpinejs.dev/) v3 framework for its UI, but instead of relying on a CDN (i.e. making unnecessary calls), a minified version (`alpine.min.js`) is included with this tool.
+
+
+## Why though?
+
+My Synapse server is small, but after joining and leaving several large rooms, using bridges, and converting my database from SQLite3 to Postgresql, there are some broken and orphaned rooms. Some of them just appear in my Element client, but I cannot leave them. Some simply exist and have not been cleaned up for some reason. At least one of these rooms is causing issues when trying to connect to Matrix using Chatty on PinePhone.
---
+++ synapse-room-cleaner/index.js
@@ -0,0 +1,88 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('room_data', () => ({
+        
+        message: '',
+        server: localStorage.getItem('server') || '', 
+        token: localStorage.getItem('token') || '', 
+        total_rooms: 0,
+        rooms: [],
+        
+        get_rooms() {
+            if (!this.server) {
+                this.message = 'Homeserver is required';
+                return;
+            }
+            if (!this.token) {
+                this.message = 'Access token is required';
+                return;
+            }
+            fetch(this.server + '/_synapse/admin/v1/rooms', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + this.token
+                }
+            })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                if (responseJson.error) {
+                    this.message = 'Error getting a list of rooms: ' + responseJson.error;
+                } else {
+                    this.total_rooms = responseJson.total_rooms;
+                    this.rooms = responseJson.rooms;
+                }
+            })
+            .catch((errorText) => {
+                this.message = 'Error getting a list of rooms: ' + errorText;
+            });
+        },
+
+        get_room_members(room_id) {
+            fetch(this.server + '/_synapse/admin/v1/rooms/' + room_id + '/members', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + this.token
+                }
+            })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                if (responseJson.error) {
+                    this.message = responseJson.error;
+                } else {
+                    this.rooms.find(room => room.room_id == room_id).members = responseJson.members;
+                }
+            })
+            .catch((errorText) => {
+                this.message = 'Error getting a list of members for room ' + room_id + ': ' + errorText;
+            });
+        },
+
+        delete_room(room_id, force) {
+            fetch(this.server + '/_synapse/admin/v1/rooms/' + room_id, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer ' + this.token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'purge': true,
+                    'force_purge': (force === true)
+                })
+            })
+            .then((response) => response.json())
+            .then((responseJson) => { 
+                if (responseJson.error)
+                {
+                    this.message = 'Error deleting room ' + room_id + ': ' + responseJson.error + '. You can try to force delete.';
+                    this.rooms.find(room => room.room_id == room_id).force_delete = true;    
+                }
+                else
+                {
+                    this.rooms.find(room => room.room_id == room_id).deleted = true;
+                }
+            })
+            .catch((errorText) => {
+                this.message = 'Error deleting room ' + room_id + ': ' + errorText;
+            });
+        }
+    }));
+});
